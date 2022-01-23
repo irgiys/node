e9a63ae2d12a620571b73a1c7f8a91ddfeb679a0
iynsys <irgiyansy@gmail.com>
2022-01-23 18:52:33 +0700

    Adding : contact-app

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,9 +1,12 @@
-const { tulisPertanyaan, simpanPertanyaan } = require("./contacts");
-const main = async () => {
-   const nama = await tulisPertanyaan("Masukan nama anda : ");
-   const email = await tulisPertanyaan("Masukan email anda : ");
-   const noHP = await tulisPertanyaan("Masukan nomor HP anda : ");
-   simpanPertanyaan(nama, email, noHP);
-};
+// mengambil argument dari command line
+const yargs = require("yargs");
+console.log(yargs.argv);
+// const { tulisPertanyaan, simpanPertanyaan } = require("./contacts");
+// const main = async () => {
+//    const nama = await tulisPertanyaan("Inputkan nama anda : ");
+//    const email = await tulisPertanyaan("Inputkan email anda : ");
+//    const noHP = await tulisPertanyaan("Inputkan nomor HP anda :");
+//    simpanPertanyaan(nama, email, noHP);
+// };
 
-main();
+// main();
--- contact-app/contacts.js
+++ contact-app/contacts.js
@@ -1,42 +1,39 @@
 const fs = require("fs");
-
 const readline = require("readline");
-const { stdin, stdout } = require("process");
 
 const rl = readline.Interface({
-   input: stdin,
-   output: stdout,
+   input: process.stdin,
+   output: process.stdout,
 });
 
-// membuat folder data
 const dirPath = "./data";
 if (!fs.existsSync(dirPath)) {
    fs.mkdirSync(dirPath);
 }
 
-// membuat file contacts.json
 const dataPath = "./data/contacts.json";
 if (!fs.existsSync(dataPath)) {
    fs.writeFileSync(dataPath, "[]", "utf-8");
 }
+
 const tulisPertanyaan = (pertanyaan) => {
-   return new Promise((resolve, reject) => {
-      rl.question(pertanyaan, (nama) => {
-         resolve(nama);
+   return new Promise((resolve, rejects) => {
+      rl.question(pertanyaan, (nanya) => {
+         resolve(nanya);
       });
    });
 };
+
 const simpanPertanyaan = (nama, email, noHP) => {
    const contact = { nama, email, noHP };
-   const file = fs.readFileSync("./data/contacts.json", "utf-8");
+   const file = fs.readFileSync(dataPath);
    const contacts = JSON.parse(file);
-   contacts.push(contact);
 
-   fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts, null, 2));
-   console.log("Terimakasih ^^");
+   contacts.push(contact);
+   fs.writeFileSync(dataPath, JSON.stringify(contacts, null, 2));
+   console.log("Thankss ^^");
    rl.close();
 };
-
 module.exports = {
    tulisPertanyaan,
    simpanPertanyaan,
